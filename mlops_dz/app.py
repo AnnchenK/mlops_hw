6c670647a513dce3c31d7e15c1eb6f9900b1878f
Annchen <aakobyakova_1@edu.hse.ru>
2024-11-10 23:21:39 +0300

    alive handle done

--- mlops_dz/app.py
+++ mlops_dz/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from typing import Literal
+import logging
 
+
+logger = logging.getLogger("uvicorn")
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/alive", summary="Aliveness check", response_model=Literal["yes", "no"])
 async def root():
-    return {"message": "Hello World"}
+    """
+      Returns "yes" if the service is alive and is ready to serve requests.
+    """
+    return "yes"
