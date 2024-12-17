386b5da3c8b8d8abaafdf30f61e401d33c64e3fa
Annchen <aakobyakova_1@edu.hse.ru>
2024-12-17 13:15:12 +0300

    fix dockerfile poetry install

--- main/run.sh
+++ main/run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-poetry install
-
 export ADDRESS=0.0.0.0 PORT=8000
 
 poetry run python mlops_dz/__init__.py &
@@ -9,10 +7,10 @@ until curl -s -o /dev/null -w "%{http_code}" "http://$ADDRESS:$PORT/alive" | gre
     sleep 1
 done
 
-dvc init --no-scm
-dvc remote add -d myremote s3://$MINIO_BUCKET
-dvc remote modify myremote endpointurl http://$MINIO_ENDPOINT
-dvc remote modify --local myremote access_key_id $MINIO_ROOT_USER
-dvc remote modify --local myremote secret_access_key $MINIO_ROOT_PASSWORD
+poetry run dvc init --no-scm
+poetry run dvc remote add -d myremote s3://$MINIO_BUCKET
+poetry run dvc remote modify myremote endpointurl http://$MINIO_ENDPOINT
+poetry run dvc remote modify --local myremote access_key_id $MINIO_ROOT_USER
+poetry run dvc remote modify --local myremote secret_access_key $MINIO_ROOT_PASSWORD
 
 poetry run streamlit run dashboard/dashboard.py >/dev/null 2>&1
